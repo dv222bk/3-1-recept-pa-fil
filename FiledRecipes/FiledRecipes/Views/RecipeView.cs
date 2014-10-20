1c63eb731cc220fe6671c7b6949daaa0029d9594
Daniel Vedin <dv222bk@student.lnu.se>
2014-10-20 22:03:56 +0200

    Finished writing Show(IRecipe recipe)

--- FiledRecipes/FiledRecipes/Views/RecipeView.cs
+++ FiledRecipes/FiledRecipes/Views/RecipeView.cs
@@ -12,5 +12,31 @@ namespace FiledRecipes.Views
     /// </summary>
     public class RecipeView : ViewBase, IRecipeView
     {
+        public void Show(IRecipe recipe)
+        {
+            base.Header = recipe.Name;
+            base.ShowHeaderPanel();
+            Console.WriteLine(Properties.Strings.Ingredients);
+            Console.WriteLine("".CenterAlignString(new String(' ', Properties.Strings.Ingredients.Length)).Replace(" ", "-"));
+            foreach (Ingredient ingredient in recipe.Ingredients)
+            {
+                Console.Write(ingredient.Amount + " ");
+                Console.Write(ingredient.Measure + " ");
+                Console.Write(ingredient.Name);
+                Console.WriteLine();
+            }
+            Console.WriteLine(Properties.Strings.Instructions);
+            Console.WriteLine("".CenterAlignString(new String(' ', Properties.Strings.Instructions.Length)).Replace(" ", "-"));
+            for (int i = 0; i < recipe.Instructions.Count(); i++)
+            {
+                Console.WriteLine("<{0}>", i + 1);
+                Console.WriteLine(recipe.Instructions.ElementAt(i));
+            }
+            base.ContinueOnKeyPressed();
+        }
+        public void Show(IEnumerable<IRecipe> recipes)
+        {
+
+        }
     }
 }
